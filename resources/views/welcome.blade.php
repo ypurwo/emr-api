1f6c1f58b742a44b3ccf813c8312f049d2cc281e
ypurwo <ypurwo@gmail.com>
2020-10-16 16:05:21 +0700

    test-made available register only for authorized user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,12 +69,13 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+
                     @endauth
                 </div>
             @endif
